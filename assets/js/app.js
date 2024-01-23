4fb1e32c9a88b2a7793968850107ae0ed9fe65fb
Bleess <TBleess@dola.state.co.us>
2024-01-23 12:14:44 -0700

    Collapsible

--- assets/js/app.js
+++ assets/js/app.js
@@ -279,7 +279,7 @@ function init() {
 
         //we calculate a bounding box equal much larger than the actual visible map.  This preloades shapes that are off the map.  Combined with the center point query, this will allow us to not have to requery the database on every map movement.
         newbounds = (coord.swlng - diff2) + "," + (coord.swlat - diff1) + "," + (coord.nelng + diff2) + "," + (coord.nelat + diff1);
-
+console.log("https://gis.dola.colorado.gov/sd/districts?limit=" + limit + "&ctf=" + ctf + "&filter=" + filter + "&bb=" + newbounds + "&zoom=" + map.getZoom() + lgid);
         geojsonLayer.refresh("https://gis.dola.colorado.gov/sd/districts?limit=" + limit + "&ctf=" + ctf + "&filter=" + filter + "&bb=" + newbounds + "&zoom=" + map.getZoom() + lgid); //add a new layer replacing whatever is there
 
     }
